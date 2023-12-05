829247bbf7af8b8aad53929f1c574bca64e827db
Satyam <satyam19arya@gmail.com>
2023-12-05 15:50:31 +0530

    updated

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -65,7 +65,7 @@ const imageUpload = async (req, res) => {
 
         // Upload to cloudinary
         const uploadedImage = await cloudinary.uploader.upload(file.tempFilePath, {
-            folder: 'FileUpload',            
+            folder: 'FileUpload',           
         });
 
         // Save to database
@@ -91,7 +91,116 @@ const imageUpload = async (req, res) => {
     }
 }
 
+const videoUpload = async (req, res) => {
+    try{
+        const {name, tags, email} = req.body;
+        const file = req.files.file;
+
+        if(!name || !tags || !email) {
+            return res.status(400).send({
+                message: 'Please fill all fields'
+            });
+        }
+
+        // Validation
+        const supportedTypes = ["mp4", "mov", "wmv", "flv", "avi", "webm"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        if(!supportedTypes.includes(fileType)) {
+            return res.status(400).send({
+                message: 'File type not supported'
+            });
+        }
+
+        if(file.size > 1024 * 1024 * 20) {
+            return res.status(400).send({
+                message: 'File size too large'
+            });
+        }
+
+        // Upload to cloudinary
+        const uploadedVideo = await cloudinary.uploader.upload(file.tempFilePath, {
+            folder: 'FileUpload', 
+            resource_type: "video",           
+        });
+
+        // Save to database
+        const newFile = new File({
+            name,
+            tags,
+            email,
+            videoUrl: uploadedVideo.secure_url,
+        });
+
+        await newFile.save();
+
+        res.status(200).json({
+            message: 'Video uploaded successfully',
+            video: uploadedVideo.secure_url
+        });
+
+    } catch(error){
+        res.status(500).send({
+            message: 'Error uploading video',
+        });
+        console.error(error);
+    }
+}
+
+const imageSizeReducer = async (req, res) => {
+    try{
+        const {name, tags, email} = req.body;
+        const file = req.files.file;
+
+        if(!name || !tags || !email) {
+            return res.status(400).send({
+                message: 'Please fill all fields'
+            });
+        }
+
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        if(!supportedTypes.includes(fileType)) {
+            return res.status(400).send({
+                message: 'File type not supported'
+            });
+        }
+
+        // Upload to cloudinary
+        const uploadedImage = await cloudinary.uploader.upload(file.tempFilePath, {
+            folder: 'FileUpload',
+            transformation: [
+                {width: 500, height: 500, crop: "limit"}
+            ],
+            quality: "40",
+            resource_type: "image",       
+        });
+
+        // Save to database
+        const newFile = new File({
+            name,
+            tags,
+            email,
+            imageUrl: uploadedImage.secure_url,
+        });
+
+        await newFile.save();
+
+        res.status(200).json({
+            message: 'Image uploaded successfully',
+            image: uploadedImage.secure_url
+        });
+
+    } catch(error){
+        res.status(500).send({
+            message: 'Error reducing image size',
+        });
+        console.error(error);
+    }
+}
+
 module.exports = {
     localFileUpload,
-    imageUpload
+    imageUpload,
+    videoUpload,
+    imageSizeReducer
 };
--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -3,5 +3,7 @@ const fileController = require('../controllers/fileController');
 
 router.post('/localFileUpload', fileController.localFileUpload);
 router.post('/imageUpload', fileController.imageUpload);
+router.post('/videoUpload', fileController.videoUpload);
+router.post('/imageSizeReducer', fileController.imageSizeReducer);
 
 module.exports = router;
