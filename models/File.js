1a6abd840b7e138a731e5594f3ef4c949a7e3667
Satyam <satyam19arya@gmail.com>
2023-12-05 16:59:09 +0530

    updated

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const fileSchema = mongoose.Schema({
     name: {
@@ -14,6 +15,30 @@ const fileSchema = mongoose.Schema({
     email: {
         type: String
     },
+});
+
+// post middleware
+fileSchema.post('save', async function(doc) {
+    console.log(doc);
+    try{
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+            }
+        });
+
+        await transporter.sendMail({
+            from: 'Satyam Arya',
+            to: doc.email,
+            subject: 'File uploaded on server',
+            html: `File uploaded successfully! <p> View it here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+        });
+
+    } catch(error) {
+        console.error(error);
+    }
 });
 
 module.exports = mongoose.model('file', fileSchema);
